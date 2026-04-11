ebe670b6f394ffc029fe75a6e9c03e4cf84ac859
Stas <stas@macmini.com>
2026-04-12 00:06:19 +0300

    first commit

---
+++ src/crypto/config.js
@@ -0,0 +1,44 @@
+import { bsc, polygon } from '@reown/appkit/networks'
+import testNetwork from './test-network.js'
+
+const blockChainsData = [
+  ...testNetwork,
+  {
+    network: bsc,
+    token: '0xD566886eB93500e2BA464bd48c8D5A2556569253',
+    receiver: '0xC08B8Ab6d6ac31374d37B26D530E1Ee5Be8d6406',
+    publicRpc: 'https://bsc-rpc.publicnode.com'
+  },
+  {
+    network: polygon,
+    token: '0xD566886eB93500e2BA464bd48c8D5A2556569253',
+    receiver: '0x9A31862285591a820EDD1c924D495880D4988D20',
+    publicRpc: 'https://polygon-bor-rpc.publicnode.com'
+  }
+]
+  .filter(({ network }) => (network.id === 14188 || network.id === 31337) ? process.env.NODE_ENV === 'development' : true)
+  .filter(({ token, receiver }) => token && receiver)
+
+const config = {
+  metadata: {
+    name: 'PayCalc',
+    description: 'Example DApp with BSC by Prohetamine - PayCalc',
+    url: 'https://calc.prohetamine.ru',
+    icons: ['https://calc.prohetamine.ru/icon.svg']
+  },
+  projectId: '1febfd92481d4ea997711d2ac4a363c0',
+  networks: blockChainsData.map(({ network }) => network),
+  address: blockChainsData.reduce((ctx, { network, token, receiver }) => {
+    ctx[network.id] = {
+      token,
+      receiver
+    }
+    return ctx
+  }, {}),
+  ABI: {
+    token: [{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"uint256","name":"_initialSupply","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"allowance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientAllowance","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientBalance","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC20InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC20InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC20InvalidSender","type":"error"},{"inputs":[{"internalType":"address","name":"spender","type":"address"}],"name":"ERC20InvalidSpender","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"_owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}],
+    receiver: [{"inputs":[{"internalType":"address","name":"_allowedToken","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"allowedToken","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"a","type":"uint256"},{"internalType":"uint256","name":"b","type":"uint256"},{"internalType":"uint8","name":"op","type":"uint8"}],"name":"calc","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"calcPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"resultCalc","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"results","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+  }
+}
+
+export default config
---
+++ src/crypto/use-crypto.js
@@ -0,0 +1,68 @@
+import { useAppKit, useAppKitAccount, useAppKitProvider, useAppKitNetwork } from '@reown/appkit/react'
+import { BrowserProvider, Contract, MaxUint256 } from 'ethers'
+import config from './config.js'
+
+const useCrypto = () => {
+    const { open } = useAppKit()
+        , { address, isConnected } = useAppKitAccount({ namespace: 'eip155' })
+        , { walletProvider } = useAppKitProvider('eip155')
+        , network = useAppKitNetwork()
+
+    const chainId = parseInt(network.chainId)
+
+    const setupSigner = async () => {
+        if (!walletProvider || !address) {
+            throw new Error('Wallet not connected')
+        }
+        const provider = new BrowserProvider(walletProvider)
+        const network = await provider.getNetwork()
+        const signer = await provider.getSigner()
+        return [signer, parseInt(network.chainId)]
+    }
+    
+
+    const getBalance = async () => {
+        const [signer, network] = await setupSigner()
+        const _address = config.address[network]
+        
+        const token = new Contract(_address.token, config.ABI.token, signer)
+        const balance = await token.balanceOf(address)
+        return balance
+    }
+
+    const calc = async (...args) => {
+        const [signer, network] = await setupSigner()
+        const _address = config.address[network]
+
+        const token = new Contract(_address.token, config.ABI.token, signer)
+            , receiver = new Contract(_address.receiver, config.ABI.receiver, signer)
+
+        const allowance = await token.allowance(address, _address.receiver)
+        const amount = 1
+
+        if (allowance < amount) {
+            const approveTx = await token.approve(_address.receiver, MaxUint256)
+            await approveTx.wait()
+        }
+
+        const receiverTx = await receiver.calc(_address.token, amount, ...args)
+        const tx = await receiverTx.wait()
+        if (tx.status === 1) {
+            const data = await receiver.resultCalc()
+            return data.toString()
+        } else {
+            return false
+        }
+    }
+
+    return { 
+        open, 
+        isConnected, 
+        getBalance, 
+        address,
+        chainId,
+        calc 
+    }
+}
+
+export default useCrypto
